6490ca8f16e0180986400e9687f5b3170661d838
Serhii Shvets <wert2all@gmail.com>
2023-07-24 16:02:31 +0300

    Add picom

--- update.sh
+++ update.sh
@@ -6,7 +6,9 @@ mkdir ~/.config/dunst -p
 mkdir ~/.config/kitty -p
 mkdir ~/.config/nitrogen -p
 mkdir ~/Pictures/Wallpapers -p
+mkdir ~/.config/picom -p
 
 cp .config/dunst/* ~/.config/dunst
 cp .config/nitrogen/* ~/.config/nitrogen
 cp wallpapers/* ~/Pictures/Wallpapers/
+cp .config/picom/* ~/.config/picom
