918888d95b4745a60077bdf47adbe6ef72d0a425
Serhii Shvets <wert2all@gmail.com>
2024-01-21 15:34:12 +0200

    Update rofi

---
+++ .config/hypr/scripts/rofilaunch.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+roconf="~/.config/rofi/config.rasi"
+
+# rofi action
+case $1 in
+    d)  r_mode="drun" ;;
+    w)  r_mode="window" ;;
+    f)  r_mode="filebrowser" ;;
+    h)  echo -e "rofilaunch.sh [action]\nwhere action,"
+        echo "d :  drun mode"
+        echo "w :  window mode"
+        echo "f :  filebrowser mode,"
+        exit 0 ;;
+    *)  r_mode="drun" ;;
+esac
+
+# launch rofi
+rofi -show $r_mode -config "${roconf}"
