bf99e9996e87e372189daa3e44e6d4010962bde6
Serhii Shvets <wert2all@gmail.com>
2024-04-29 16:06:44 +0300

    feat: swww random wallpapers (#67)

--- .config/hypr/scripts/swwwallpaper.sh
+++ .config/hypr/scripts/swwwallpaper.sh
@@ -10,24 +10,16 @@ fi
 #Get a list of all image files in the wallpapers directory
 wallpapers=("$wallPath"/*)
 
-# Start an infinite loop
-while true; do
-    # Check if the wallpapers array is empty
-    if [ ${#wallpapers[@]} -eq 0 ]; then
-        # If the array is empty, refill it with the image files
-        wallpapers=("$wallPath"/*)
-    fi
+# Check if the wallpapers array is empty
+if [ ${#wallpapers[@]} -eq 0 ]; then
+    # If the array is empty, refill it with the image files
+    wallpapers=("$wallPath"/*)
+fi
 
-    # Select a random wallpaper from the array
-    wallpaperIndex=$(( RANDOM % ${#wallpapers[@]} ))
-    selectedWallpaper="${wallpapers[$wallpaperIndex]}"
+# Select a random wallpaper from the array
+wallpaperIndex=$(( RANDOM % ${#wallpapers[@]} ))
+selectedWallpaper="${wallpapers[$wallpaperIndex]}"
 
-    # Update the wallpaper using the swww img command
-    swww img "$selectedWallpaper" --transition-type "wipe" --transition-duration 2
+# Update the wallpaper using the swww img command
+swww img "$selectedWallpaper" --transition-type "wipe" --transition-duration 2
 
-    # Remove the selected wallpaper from the array
-    unset "wallpapers[$wallpaperIndex]"
-
-    # Delay for 1 hours before selecting the next wallpaper
-    sleep 1h
-done
