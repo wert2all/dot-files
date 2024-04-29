a56157e337f26639f900f735381c2abb105355a5
Serhii Shvets <wert2all@gmail.com>
2024-04-29 16:06:02 +0300

    feat: swww random wallpapers

--- .config/hypr/scripts/swwwallpaper.sh
+++ .config/hypr/scripts/swwwallpaper.sh
@@ -2,13 +2,32 @@
 
 wallPath="${XDG_CONFIG_HOME:-$HOME/.config}/swww"
 
-# nohup swww-daemon >&/dev/null &
-
 swww query
 if [ $? -eq 1 ]; then
   swww init
 fi
 
-swww img "${wallPath}/current.set" \
-   --transition-type "wipe" \
-   --transition-duration 2
+#Get a list of all image files in the wallpapers directory
+wallpapers=("$wallPath"/*)
+
+# Start an infinite loop
+while true; do
+    # Check if the wallpapers array is empty
+    if [ ${#wallpapers[@]} -eq 0 ]; then
+        # If the array is empty, refill it with the image files
+        wallpapers=("$wallPath"/*)
+    fi
+
+    # Select a random wallpaper from the array
+    wallpaperIndex=$(( RANDOM % ${#wallpapers[@]} ))
+    selectedWallpaper="${wallpapers[$wallpaperIndex]}"
+
+    # Update the wallpaper using the swww img command
+    swww img "$selectedWallpaper" --transition-type "wipe" --transition-duration 2
+
+    # Remove the selected wallpaper from the array
+    unset "wallpapers[$wallpaperIndex]"
+
+    # Delay for 1 hours before selecting the next wallpaper
+    sleep 1h
+done
