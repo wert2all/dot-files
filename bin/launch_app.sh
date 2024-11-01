b4e8d64d01c59243dad6b9a35e2e4c0343414464
wert2all <wert2all@gmail.com>
2024-11-01 12:15:42 +0200

    fix: update obsidian

--- bin/launch_app.sh
+++ bin/launch_app.sh
@@ -3,6 +3,6 @@
 case $1 in
 sql) ~/apps/Beekeeper-Studio-4.3.4.AppImage ;;
 api) ~/apps/HTTPie-2024.1.2.AppImage ;;
-obsidian) ~/apps/Obsidian-1.5.12.AppImage ;;
+obsidian) ~/apps/Obsidian-1.7.4.AppImage ;;
 *) print_error ;;
 esac
