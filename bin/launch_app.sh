9a0fa708be75222461c3abc3c5df3b08e5a37360
Serhii Shvets <wert2all@gmail.com>
2024-11-12 00:08:10 +0200

    fix: update launch script

--- bin/launch_app.sh
+++ bin/launch_app.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 case $1 in
-sql) ~/apps/Beekeeper-Studio-4.3.4.AppImage ;;
+sql) ~/apps/Beekeeper-Studio-5.0.0-beta.2.AppImage ;;
 api) ~/apps/HTTPie-2024.1.2.AppImage ;;
 obsidian) ~/apps/Obsidian-1.7.4.AppImage ;;
 *) print_error ;;
