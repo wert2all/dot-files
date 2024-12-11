e743539c4f8194571fdb3e91903ba4edfd7796bf
Serhii Shvets <wert2all@gmail.com>
2024-12-12 01:53:54 +0200

    feat: add uvim config

---
+++ .config/uvim/lua/plugins/lsp.lua
@@ -0,0 +1,46 @@
+return {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+        { "williamboman/mason.nvim", config = true },
+        "williamboman/mason-lspconfig.nvim",
+        {
+            -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+            -- used for completion, annotations and signatures of Neovim apis
+            'folke/lazydev.nvim',
+            ft = 'lua',
+            opts = {
+                library = {
+                    -- Load luvit types when the `vim.uv` word is found
+                    { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+                },
+            },
+        },
+    },
+
+    config = function()
+        require("mason").setup({
+            ui = {
+                border = "rounded",
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗",
+                },
+            },
+        })
+        require("mason-lspconfig").setup({
+            ensure_installed = { "lua_ls" },
+            automatic_installation = true,
+        })
+
+        require('mason-lspconfig').setup_handlers({
+            function(server_name)
+                require("lspconfig")[server_name].setup({})
+            end,
+        })
+    end,
+    keys = {
+        { "<leader>lf", function() vim.lsp.buf.format() end, desc = "Format buffer" }
+    }
+}
---
+++ .config/uvim/lua/plugins/which-key.lua
@@ -0,0 +1,9 @@
+return {
+    {
+        "folke/which-key.nvim",
+        dependencies = {
+            { 'echasnovski/mini.icons', version = false }
+        },
+        event = "VeryLazy",
+    }
+}
