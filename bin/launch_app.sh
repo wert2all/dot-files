8950031653abf45367aea40b73d03d55f4c347b1
Serhii Shvets <wert2all@gmail.com>
2025-01-10 10:11:27 +0200

    fix: update BeeKeeperStudio

--- bin/launch_app.sh
+++ bin/launch_app.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 case $1 in
-sql) ~/apps/Beekeeper-Studio-5.0.0-beta.5.AppImage ;;
+sql) ~/apps/Beekeeper-Studio-5.0.9.AppImage ;;
 api) ~/apps/HTTPie-2024.1.2.AppImage ;;
 obsidian) ~/apps/Obsidian-1.7.4.AppImage ;;
 *) print_error ;;
