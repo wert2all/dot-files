9ba20bfee01b58cc3b1249f92d711141a83dd600
Serhii Shvets <wert2all@gmail.com>
2025-02-06 23:29:01 +0200

    fix: fix HTTPie launch

--- bin/launch_app.sh
+++ bin/launch_app.sh
@@ -2,7 +2,7 @@
 
 case $1 in
 sql) ~/apps/Beekeeper-Studio-5.0.9.AppImage ;;
-api) ~/apps/HTTPie-2024.1.2.AppImage ;;
+api) ~/apps/HTTPie-2025.1.0.AppImage ;;
 obsidian) ~/apps/Obsidian-1.7.4.AppImage ;;
 *) print_error ;;
 esac
