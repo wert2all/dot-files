cb93de548af52739fccd5f1190f9894289565452
Serhii Shvets <wert2all@gmail.com>
2025-02-18 12:01:18 +0200

    feat: add bruno to launch script

--- bin/launch_app.sh
+++ bin/launch_app.sh
@@ -4,5 +4,6 @@ case $1 in
 sql) ~/apps/Beekeeper-Studio-5.0.9.AppImage ;;
 api) ~/apps/HTTPie-2025.1.0.AppImage ;;
 obsidian) ~/apps/Obsidian-1.7.4.AppImage ;;
+bruno) ~/apps/bruno_1.39.0_x86_64_linux.AppImage ;;
 *) print_error ;;
 esac
