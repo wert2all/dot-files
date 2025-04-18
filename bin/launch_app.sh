421ff848da2a85ee235ffa11e859f91f8edcf7b1
wert2all <wert2all@gmail.com>
2025-04-18 11:34:44 +0300

    fix: update version of HTTPie

--- bin/launch_app.sh
+++ bin/launch_app.sh
@@ -2,7 +2,7 @@
 
 case $1 in
 sql) ~/apps/Beekeeper-Studio-5.0.9.AppImage ;;
-api) ~/apps/HTTPie-2025.1.0.AppImage ;;
+api) ~/apps/HTTPie-2025.2.0.AppImage ;;
 obsidian) ~/apps/Obsidian-1.7.4.AppImage ;;
 bruno) ~/apps/bruno_1.39.0_x86_64_linux.AppImage ;;
 *) print_error ;;
