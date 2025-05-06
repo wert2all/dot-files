fcb27509ae29f00b6ddc357b25f20de6d16f3c6e
Serhii Shvets <wert2all@gmail.com>
2025-05-06 16:01:34 +0300

    feat(apps): update app versions

--- bin/launch_app.sh
+++ bin/launch_app.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
 case $1 in
-sql) ~/apps/Beekeeper-Studio-5.0.9.AppImage ;;
+sql) ~/apps/Beekeeper-Studio-5.2.2.AppImage ;;
 api) ~/apps/HTTPie-2025.2.0.AppImage ;;
 obsidian) ~/apps/Obsidian-1.7.4.AppImage ;;
-bruno) ~/apps/bruno_1.39.0_x86_64_linux.AppImage ;;
+bruno) ~/apps/bruno_2.2.0_x86_64_linux.AppImage ;;
 *) print_error ;;
 esac
