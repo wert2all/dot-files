9c9d3d9d7a592f66ce44ad5ea1fa33c2d7657aab
Serhii Shvets <wert2all@gmail.com>
2025-07-16 02:01:14 +0300

    fix: fix volume control script

--- bin/volumecontrol.sh
+++ bin/volumecontrol.sh
@@ -1,32 +1,59 @@
 #!/usr/bin/env sh
 
-# define functions
-source "$HOME"/bin/_functions.sh
-
-function send_notification {
-    vol=$1
-    angle="$(( (($vol+2)/5) * 5 ))"
-    ico="${icodir}/vol-${angle}.svg"
-    bar=$(seq -s "." $(($vol / 15)) | sed 's/[0-9]//g')
-    dunstify "t2" -a "$vol$bar" "$nsink" -i $ico -r 91190 -t 800
-}
-
-function get_volume()
-{
-  pactl get-sink-volume @DEFAULT_SINK@ | grep -Po '\d+(?=%)' | head -n 1
-}
-
 icodir=$HOME"/.config/dunst/icons/vol"
 
-case $1 in
-    i) pactl set-sink-volume @DEFAULT_SINK@ +5%
-       send_notification $(get_volume) ;;
-    d) pactl set-sink-volume @DEFAULT_SINK@ -5%
-       send_notification $(get_volume) ;;
-    m) pactl set-sink-mute @DEFAULT_SINK@ toggle
-       notify_mute "speaker";;
-    mm) pactl set-source-mute @DEFAULT_SOURCE@ toggle
-        notify_mute "mic";;
-    *) print_error ;;
-esac
+send_notification() {
+    dunstify "t2" -a "${1}" -i "${2}" -r 91190 -t 800
+}
 
+mute() {
+    device=$1
+
+    case $device in
+    mic)
+        pactlCommand="get-source-mute"
+        dsink="@DEFAULT_SOURCE@"
+        ;;
+    speaker)
+        pactlCommand="get-sink-mute"
+        dsink="@DEFAULT_SINK@"
+        ;;
+    esac
+
+    mute=$(pactl "${pactlCommand}" "${dsink}" | cut -d ' ' -f 2)
+
+    if [ "$mute" = "yes" ]; then
+        send_notification "muted" "${icodir}/muted-${device}.svg"
+    else
+        send_notification "unmuted" "${icodir}/unmuted-${device}.svg"
+    fi
+}
+
+say_volume() {
+    angle="$(((($1 + 2) / 5) * 5))"
+    send_notification "${1}..." "${icodir}/vol-${angle}.svg"
+}
+
+get_volume() {
+    pactl get-sink-volume @DEFAULT_SINK@ | grep -Po '\d+(?=%)' | head -n 1
+}
+
+case $1 in
+i)
+    pactl set-sink-volume @DEFAULT_SINK@ +5%
+    say_volume "$(get_volume)"
+    ;;
+d)
+    pactl set-sink-volume @DEFAULT_SINK@ -5%
+    say_volume "$(get_volume)"
+    ;;
+m)
+    pactl set-sink-mute @DEFAULT_SINK@ toggle
+    mute "speaker"
+    ;;
+mm)
+    pactl set-source-mute @DEFAULT_SOURCE@ toggle
+    mute "mic"
+    ;;
+*) printf "use i,d,m,mm" ;;
+esac
