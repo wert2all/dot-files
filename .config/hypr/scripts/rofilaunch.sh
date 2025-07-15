31e443ea1463165f30ccc385c43610c33e066e6f
Serhii Shvets <wert2all@gmail.com>
2025-07-16 01:46:24 +0300

    fix: format scripts

--- .config/hypr/scripts/rofilaunch.sh
+++ .config/hypr/scripts/rofilaunch.sh
@@ -4,15 +4,17 @@ roconf="~/.config/rofi/config.rasi"
 
 # rofi action
 case $1 in
-    d)  r_mode="drun" ;;
-    w)  r_mode="window" ;;
-    f)  r_mode="filebrowser" ;;
-    h)  echo -e "rofilaunch.sh [action]\nwhere action,"
-        echo "d :  drun mode"
-        echo "w :  window mode"
-        echo "f :  filebrowser mode,"
-        exit 0 ;;
-    *)  r_mode="drun" ;;
+d) r_mode="drun" ;;
+w) r_mode="window" ;;
+f) r_mode="filebrowser" ;;
+h)
+    echo -e "rofilaunch.sh [action]\nwhere action,"
+    echo "d :  drun mode"
+    echo "w :  window mode"
+    echo "f :  filebrowser mode,"
+    exit 0
+    ;;
+*) r_mode="drun" ;;
 esac
 
 # launch rofi
