b107fe9b6828f8155c1c9102e902e41b88ef1bb2
Serhii Shvets <wert2all@gmail.com>
2025-07-31 23:23:13 +0300

    fix: fix ssh agent init

--- bin/init_ssh_agent.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-SSH_ENV="$HOME/.ssh/agent.env"
-
-echo "Initialising new SSH agent..."
-ssh-agent -s | sed 's/^echo/#echo/' >"${SSH_ENV}"
-chmod 600 "${SSH_ENV}"
-. "${SSH_ENV}" >/dev/null
