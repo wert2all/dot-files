17faa17e272e5a22d3afa08bd2215e0931822114
Serhii Shvets <wert2all@gmail.com>
2025-10-27 15:37:00 +0200

    fix: remove exposrt ai keys on zsh start up

---
+++ bin/init_api_keys.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+export MISTRAL_API_KEY=$(pass show api/ai/mistral)
+export GEMINI_API_KEY=$(pass show api/ai/gemini)
+export OPENROUTER_API_KEY=$(pass show api/ai/openrouter)
+export ANTHROPIC_API_KEY=$(pass show api/ai/claude)
