6b3306bf2b582f620017928cc5ba5c8c452c5270
Serhii Shvets <wert2all@gmail.com>
2026-01-10 12:15:31 +0200

    fix: disable load some api keys which i do not use often

--- bin/init_api_keys.sh
+++ bin/init_api_keys.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
 
+# export GEMINI_API_KEY=$(pass show api/ai/gemini)
+# export ANTHROPIC_API_KEY=$(pass show api/ai/claude)
+# export CROWDIN_API_TOKEN=$(pass show api/crowdin)
+
 export MISTRAL_API_KEY=$(pass show api/ai/mistral)
-export GEMINI_API_KEY=$(pass show api/ai/gemini)
 export OPENROUTER_API_KEY=$(pass show api/ai/openrouter)
-export ANTHROPIC_API_KEY=$(pass show api/ai/claude)
 export OPENCODE_API_KEY=$(pass show api/ai/opencode_zen)
-export CROWDIN_API_TOKEN=$(pass show api/crowdin)
 
 export API_KEYS_LOADED=1
