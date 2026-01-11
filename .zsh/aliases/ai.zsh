69fb53f5748c8a6beee196b76329ae3e19d249d3
Serhii Shvets <wert2all@gmail.com>
2026-01-11 14:43:15 +0200

    fix: fix fabric alias for arch

--- .zsh/aliases/ai.zsh
+++ .zsh/aliases/ai.zsh
@@ -2,6 +2,6 @@
 alias '?'='noglob question'
 alias gencom='oc run "$(cat ~/.zsh/ai/generate_commit.md)"'
 
-if [ ! command -v fabric ] >/dev/null 2>&1; then
+if ! command -v fabric >/dev/null 2>&1; then
   alias fabric='fabric-ai'
 fi
