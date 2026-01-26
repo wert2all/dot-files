da65d5dacc356f6b1e917e4febd0fcccc2bcf558
Serhii Shvets <wert2all@gmail.com>
2026-01-27 00:25:31 +0200

    feat: add aihubmix key

--- bin/init_api_keys.sh
+++ bin/init_api_keys.sh
@@ -7,5 +7,6 @@
 export MISTRAL_API_KEY=$(pass show api/ai/mistral)
 export OPENROUTER_API_KEY=$(pass show api/ai/openrouter)
 export OPENCODE_API_KEY=$(pass show api/ai/opencode_zen)
+export AIHUBMIX_API_KEY=$(pass show api/ai/ai_hub_mix)
 
 export API_KEYS_LOADED=1
