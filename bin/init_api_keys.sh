bf0a5a9837a71cba334bb64e16242781609b9877
Serhii Shvets <wert2all@gmail.com>
2026-01-30 20:11:21 +0200

    fix: disable aihubmax temporaly

--- bin/init_api_keys.sh
+++ bin/init_api_keys.sh
@@ -3,10 +3,10 @@
 # export GEMINI_API_KEY=$(pass show api/ai/gemini)
 # export ANTHROPIC_API_KEY=$(pass show api/ai/claude)
 # export CROWDIN_API_TOKEN=$(pass show api/crowdin)
+# export AIHUBMIX_API_KEY=$(pass show api/ai/ai_hub_mix)
 
 export MISTRAL_API_KEY=$(pass show api/ai/mistral)
 export OPENROUTER_API_KEY=$(pass show api/ai/openrouter)
 export OPENCODE_API_KEY=$(pass show api/ai/opencode_zen)
-export AIHUBMIX_API_KEY=$(pass show api/ai/ai_hub_mix)
 
 export API_KEYS_LOADED=1
