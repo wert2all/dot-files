262d425dae08946cedb2acb55bd436257582f73c
Serhii Shvets <wert2all@gmail.com>
2026-02-08 02:49:43 +0200

    feat: add context7

--- bin/init_api_keys.sh
+++ bin/init_api_keys.sh
@@ -8,5 +8,6 @@
 export MISTRAL_API_KEY=$(pass show api/ai/mistral)
 export OPENROUTER_API_KEY=$(pass show api/ai/openrouter)
 export OPENCODE_API_KEY=$(pass show api/ai/opencode_zen)
+export CONTEXT7_API_KEY=$(pass show api/ai/context7)
 
 export API_KEYS_LOADED=1
