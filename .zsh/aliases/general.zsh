8839e5dea1cfb75830be5355dc896f6a23bccacd
Serhii Shvets <wert2all@gmail.com>
2026-02-23 11:30:16 +0200

    fix: small fixes

--- .zsh/aliases/general.zsh
+++ .zsh/aliases/general.zsh
@@ -3,7 +3,7 @@ alias vim=nvim
 alias nvchad="NVIM_APPNAME=nvchad nvim"
 
 # Utility aliases
-alias cat="bat --theme-dark default --theme-light GitHub"
+alias cat="bat --theme default"
 alias dus="sudo du -hs \$(ls -A) | sort -h"
 alias ll="eza -lh --icons=auto --sort=name --group-directories-first"
 alias code="~/code/code"
