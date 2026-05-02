ee33787a1467d157eb648a17482c93739258f23c
Serhii Shvets <wert2all@gmail.com>
2026-05-02 13:24:52 +0300

    fix: update pi models

--- bin/init_api_keys.sh
+++ bin/init_api_keys.sh
@@ -11,5 +11,6 @@ export OPENCODE_API_KEY=$(pass show api/ai/opencode_zen)
 export CONTEXT7_API_KEY=$(pass show api/ai/context7)
 export GITHUB_TOKEN=$(pass show token/github/opencode)
 export NVIDIA_API_KEY=$(pass show api/ai/nvidia)
+export POOLSIDE_API_KEY=$(pass show api/ai/poolside)
 
 export API_KEYS_LOADED=1
