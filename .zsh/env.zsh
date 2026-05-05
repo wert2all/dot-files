fad4c3178b230956e98b397f93755a0e49faa2f3
Serhii Shvets <wert2all@gmail.com>
2026-05-05 22:53:33 +0300

    chore: migrate to opencode-go provider and update defaults

--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -46,5 +46,5 @@ export GPG_TTY=$(tty)
 
 # pi codding agent
 export PI_NOTIFY_SOUND_CMD='paplay /usr/share/sounds/freedesktop/stereo/complete.oga'
-export PI_QUICK_PROVIDER='nvidia'
-export PI_QUICK_MODEL='openai/gpt-oss-120b'
+export PI_QUICK_PROVIDER='opencode-go'
+export PI_QUICK_MODEL='deepseek-v4-flash'
